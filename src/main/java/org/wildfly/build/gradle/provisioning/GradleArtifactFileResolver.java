1cfdedd75cf16a50800b11027165dd56b0c08611
Sanne Grinovero <sanne@hibernate.org>
2018-01-20 15:09:23 +0000

    Adding the JBoss Nexus repository automatically in case of need

--- src/main/java/org/wildfly/build/gradle/provisioning/GradleArtifactFileResolver.java
+++ src/main/java/org/wildfly/build/gradle/provisioning/GradleArtifactFileResolver.java
@@ -6,7 +6,9 @@ import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.ResolvedConfiguration;
 import org.gradle.api.artifacts.dsl.DependencyHandler;
+import org.gradle.api.artifacts.dsl.RepositoryHandler;
 
 import org.wildfly.build.ArtifactFileResolver;
 import org.wildfly.build.pack.model.Artifact;
@@ -19,9 +21,14 @@ import org.wildfly.build.pack.model.Artifact;
  */
 public class GradleArtifactFileResolver implements ArtifactFileResolver {
 
+	private static final String JBOSS_REPOSITORY = "http://repository.jboss.org/nexus/content/groups/public/";
+
 	private final Project project;
 	private final DependencyHandler dependencies;
 	private final ConfigurationContainer configurations;
+
+	//We set this flag when we enable additional repositories automatically.
+	private boolean repositoriesAutomaticallyAdded = false;
 
 	public GradleArtifactFileResolver(Project project) {
 		this.project = project;
@@ -37,7 +44,7 @@ public class GradleArtifactFileResolver implements ArtifactFileResolver {
 	@Override
 	public File getArtifactFile(Artifact artifact) {
 		final Dependency dependency = asGradleDependency( artifact );
-		return createTemporaryConfiguration( dependency ).getSingleFile();
+		return createTemporaryConfiguration( dependency, artifact ).getSingleFile();
 	}
 
 	private Dependency asGradleDependency(Artifact artifact) {
@@ -46,13 +53,36 @@ public class GradleArtifactFileResolver implements ArtifactFileResolver {
 		return dependencies.create( gradleNotation );
 	}
 
-	private Configuration createTemporaryConfiguration(Dependency dependency) {
+	private Configuration createTemporaryConfiguration(Dependency dependency, Artifact artifact) {
 		Configuration config = configurations.detachedConfiguration( dependency );
-		//TODO Should we extend a well known configuration to allow ad-hoc setting of repositories?
-		//config.setExtendsFrom(  )
 		config.setTransitive( false );
 		config.setVisible( false );
+		//This next line actually resolves it (Attempts to download dependencies):
+		final ResolvedConfiguration resolvedConfiguration = config.getResolvedConfiguration();
+		if ( resolvedConfiguration.hasError() ) {
+			//In case of error, user is likely not having the JBoss Nexus repository enabled.
+			//Attempt configuration correction:
+			if ( repositoriesAutomaticallyAdded == false ) {
+				repositoriesAutomaticallyAdded = true;
+				addDefaultRepositories( artifact );
+				//And retry:
+				return createTemporaryConfiguration( dependency, artifact );
+			}
+			else {
+				resolvedConfiguration.rethrowFailure();
+			}
+		}
 		return config;
+	}
+
+	private void addDefaultRepositories(Artifact artifact) {
+		project.getLogger().warn( "Error fetching WildFly component '{}' from configured repositories; check your repository configurations to prevent this problem.\nAttempting to resolve this by enabling additional repositories automatically! Adding: [mavenLocal(), mavenCentral() and {}]", artifact, JBOSS_REPOSITORY );
+		final RepositoryHandler repositories = project.getRepositories();
+		repositories.mavenLocal();
+		repositories.mavenCentral();
+		repositories.maven( mavenArtifactRepository -> {
+			mavenArtifactRepository.setUrl( JBOSS_REPOSITORY );
+		} );
 	}
 
 }
