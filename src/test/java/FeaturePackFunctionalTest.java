5f78d3c1af1e0d9eedb920d0c75f9db3a0eee4a1
Andrea Boriero <andrea@hibernate.org>
2018-01-26 18:13:59 +0000

    Improved FeaturePackFunctionalTest

--- src/test/java/FeaturePackFunctionalTest.java
+++ src/test/java/FeaturePackFunctionalTest.java
@@ -14,7 +14,12 @@ import org.gradle.testkit.runner.TaskOutcome;
 
 import org.junit.Test;
 
+import org.wildfly.build.util.ModuleParseResult;
+import org.wildfly.build.util.ModuleParser;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -23,24 +28,30 @@ import static org.junit.Assert.assertTrue;
 public class FeaturePackFunctionalTest extends BaseFunctionalTest {
 
 	@Test
-	public void testFeaturePackTask() throws IOException {
+	public void testFeaturePackTask() throws Exception {
 		copyTemplateModulesForTesting();
 
 		BuildResult result = runTask( "featurepack" );
 
 		assertEquals( result.task( ":featurepack" ).getOutcome(), TaskOutcome.SUCCESS );
 
-		File featureFiles = new File( getWorkDirectory()
-											  + File.separator + "featurepack"
-											  + File.separator + "modules"
-											  + File.separator + "system"
-											  + File.separator + "layers"
-											  + File.separator + "base"
-											  + File.separator + "org"
-											  + File.separator + "hibernate"
-											  + File.separator + "orm53"
-											  + File.separator + "module.xml" );
-		assertTrue( "The module.xml has not been created " + featureFiles.getPath(), featureFiles.exists() );
+		File expectedModuleFile = new File( getWorkDirectory()
+													+ File.separator + "featurepack"
+													+ File.separator + "modules"
+													+ File.separator + "system"
+													+ File.separator + "layers"
+													+ File.separator + "base"
+													+ File.separator + "org"
+													+ File.separator + "hibernate"
+													+ File.separator + "orm53"
+													+ File.separator + "module.xml" );
+		assertTrue(
+				"The module.xml has not been created " + expectedModuleFile.getPath(),
+				expectedModuleFile.exists()
+		);
+
+		ModuleParseResult parseResult = ModuleParser.parse( expectedModuleFile.toPath() );
+		assertThat( parseResult.getIdentifier().getSlot(), is("orm53"));
 	}
 
 	private void copyTemplateModulesForTesting() throws IOException {
@@ -49,6 +60,8 @@ public class FeaturePackFunctionalTest extends BaseFunctionalTest {
 		File moduleTemplate = new File( examplemodulesDirectory.toString() + File.separator + "hibernateorm-module.xml" );
 		moduleTemplate.createNewFile();
 		writeFile( moduleTemplate, loadFileContent( "hibernateorm-module.xml" ) );
+
+
 	}
 
 	@Override
